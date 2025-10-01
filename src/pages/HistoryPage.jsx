7276405f1871f840b0438918bc5301eb257e87ec
GaelANM999 <a222209480@unison.mx>
2025-10-01 12:07:23 -0700

    chores: filtros
    le metí el combo pero no hace nada

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -11,7 +11,7 @@ import { uploadImage } from "../services/storage.service";
 import { getCategorias } from "../services/categoria.service";
 import { getProductos, postProducto } from "../services/producto.service";
 import { useQuery } from "@tanstack/react-query";
-import { option } from "framer-motion/client"; 
+import { option } from "framer-motion/client";
 import { getOrdenes } from "../services/orden.service";
 
 const HistoryPage = () => {
@@ -24,70 +24,91 @@ const HistoryPage = () => {
     queryFn: getOrdenes,
   });
 
-  const [showSelectedNombreCliente, setShowSelectedNombreCliente] = useState(false);
   const [search, setSearch] = useState("");
-  const [showDateTime, setShowDateTime] = useState(false);
   const [showEstado, setShowEstado] = useState(false);
   const [showTotal, setShowTotal] = useState(false);
   const [showEmpleado, setShowEmpleado] = useState(false);
 
-  const ordenesCompletadas = 
+  const ordenesTerminadas =
     historial?.filter((orden) => {
       const matchSearch = orden.nombreCliente
         .toLowerCase()
         .includes(search.toLowerCase());
 
-      const matchState = orden.estado === "Cancelada" 
-      || orden.estado === "Completada";
+      const matchState = orden.estado === "Cancelada"
+        || orden.estado === "Completada";
 
       return matchSearch && matchState;
     }) || [];
 
- 
+  const ordenesCompletadas =
+    historial?.filter((orden) => orden.estado === "Completada") || [];
+
+  const ordenesCanceladas =
+    historial?.filter((orden) => orden.estado === "Cancelada") || [];
+
 
 
   return (
     <div className="w-full flex-1 flex flex-col items-center justify-center">
       <div className="flex flex-row p-4 w-[90%]">
         <h2 className="text-2xl font-bold ">Historial de Órdenes</h2>
-        <SearchBox 
+        <SearchBox
           value={search}
           onChange={(e) => setSearch(e.target.value)} />
+        <select
+          className="bg-white border border-black rounded-md p-2 px-4 hover:bg-gray-100 font-semibold"
+          name="categoria"
+          id="categoria"
+        >
+          <option value="Categoria">Estado</option>
+          <option value={ordenesCompletadas}>Completadas</option>
+          <option value={ordenesCanceladas}>Canceladas</option>
+        </select>
+        <select
+          className="bg-white border border-black rounded-md p-2 px-4 hover:bg-gray-100 font-semibold"
+          name="categoria"
+          id="categoria"
+        >
+          <option value="Categoria">Empleado</option>
+          <option value="Bebidas">Bebidas</option>
+          <option value="Alimentos">Alimentos</option>
+        </select>
       </div>
       <div className="w-full flex flex-col items-center">
-      <div className="w-[90%] overflow-x-auto bg-white border rounded-md shadow-md">
-        <table className="w-full border-collapse">
-          <thead>
-            <tr className="bg-gray-200 text-left">
-              <th className="p-3">Nombre Cliente</th>
-              <th className="p-3">Fecha y Hora</th>
-              <th className="p-3">Estado</th>
-              <th className="p-3">Total</th>
-              <th className="p-3">Empleado</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordenesCompletadas.map((orden) => (
-              <tr
-                key={orden.id}
-                className="border-t hover:bg-gray-100 transition"
-              >
-                <td className="p-3">
-                  {orden.nombreCliente}
-                  </td>
-                <td className="p-3">
-                  {new Date(orden.fechaHora).toLocaleString("es-MX")}
-                </td>
-                <td className="p-3">{orden.estado}</td>
-                <td className="p-3 font-semibold">
-                ${orden.total.toFixed(2)}</td>
-                <td className="p-3">{orden.idUsuario?.nombre || "N/A"}</td>
+        <div className="w-[90%] overflow-x-auto bg-white border rounded-md shadow-md">
+          <table className="w-full border-collapse">
+            <thead>
+              <tr className="bg-gray-200 text-left">
+                <th className="p-3">Nombre Cliente</th>
+                <th className="p-3">Fecha y Hora</th>
+                <th className="p-3">Estado</th>
+                <th className="p-3">Total</th>
+                <th className="p-3">Empleado</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {ordenesTerminadas.map((orden) => (
+                <tr
+                  key={orden.id}
+                  className="border-t hover:bg-gray-100 transition"
+                >
+                  <td className="p-3">
+                    {orden.nombreCliente}
+                  </td>
+                  <td className="p-3">
+                    {new Date(orden.fechaHora).toLocaleString("es-MX")}
+                  </td>
+                  <td className="p-3">{orden.estado}</td>
+                  <td className="p-3 font-semibold">
+                    ${orden.total.toFixed(2)}</td>
+                  <td className="p-3">{orden.idUsuario?.nombre || "N/A"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
